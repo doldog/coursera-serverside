cafa4a55eb686353462f4743b30960452f964580
doldog <davekoukai@gmail.com>
2018-12-21 13:00:59 -0800

    template

---
+++ index.js
@@ -0,0 +1,19 @@
+const rect = {
+        perimeter: (x, y) => (2 * (x + y)),
+        area: (x, y) => (x * y)
+};
+
+let solveRect = function(l,w){
+    console.log(`Solving for rectangle with l =  ${l} w = ${w}` );
+    if (l <= 0 || w <= 0) console.log("wrong dimension");
+    else {
+        console.log(`The area of the rect is ${rect.area(l,w)}`);
+        console.log(`The perimeter of the rect is ${rect.perimeter(l,w)}`);
+    }
+};
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-1,10);
+
